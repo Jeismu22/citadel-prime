b884359f8657b2cb2398dcdd9f630b7dcf4f9194
Jeismu22 <j.rodriguezm21@curnvirtual.edu.co>
2024-11-06 11:25:48 -0500

    Templates modificados para mostrar post desde base de datos.

--- citadel_Inicia/views.py
+++ citadel_Inicia/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post  # Importamos el modelo Post
 
 def post_list(request):
-    return render(request, 'citadel_Inicia/post_list.html', {})
-
-# Create your views here.
+    # Obtener todos los posts publicados, ordenados por fecha de publicación
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    
+    # Pasar los posts a la plantilla como parte del contexto
+    return render(request, 'citadel_Inicia/post_list.html', {'posts': posts})
