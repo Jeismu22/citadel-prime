2e6e06e806cb957cc59d240a7aa30183b46cfe2f
Jeismu22 <j.rodriguezm21@curnvirtual.edu.co>
2024-11-22 08:34:38 -0500

    Mensaje descriptivo de los cambios

--- citadel/wsgi.py
+++ citadel/wsgi.py
@@ -7,14 +7,20 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+
 import os
 import sys
 
-path = '/home/munozjei2/munozjei2.pythonanywhere.com'
-if path not in sys.path:
-    sys.path.append(path)
+# Añadir el directorio de tu proyecto al path
+project_home = '/home/munozjei2/citadel-prime'  # Ruta donde está tu proyecto
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'nombre_de_tu_proyecto.settings'
+# Establecer el entorno virtual
+activate_this = '/home/munozjei2/citadel-prime/myenv/bin/activate_this.py'
+exec(open(activate_this).read(), {'__file__': activate_this})
 
+# Configurar Django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'myproject.settings'  # Ajusta esto a tu archivo settings.py
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
