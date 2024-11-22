93a51f2ff2db593b2dfcb29080dc6d9a2cc18630
Jeismu22 <j.rodriguezm21@curnvirtual.edu.co>
2024-11-22 07:34:39 -0500

    Primer commit

--- citadel/settings.py
+++ citadel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n^-=mf^d=v+4nfemfvh!p9g!-ic=7bu^96-u^t94&nn2_u^!-9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'munozjei2.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 
