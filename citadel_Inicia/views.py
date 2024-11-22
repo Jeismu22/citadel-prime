d98366a5c7374009a6daf67ebddea966f782b253
Jeismu22 <j.rodriguezm21@curnvirtual.edu.co>
2024-11-21 19:04:36 -0500

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- citadel_Inicia/views.py
+++ citadel_Inicia/views.py
@@ -1,7 +1,11 @@
 # citadel_Inicia/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
     posts = Post.objects.all()  # Obtener todos los posts
     return render(request, 'ciudadela/post_list.html', {'posts': posts})  # Cambia 'citadel_Inicia' a 'ciudadela'
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)  # Buscamos la publicación por su pk
+    return render(request, 'blog/post_detail.html', {'post': post})
